c15fc8aa5a049bad56f4de29fc200d3dcf973a11
Yuchao <yuchaozheng@ucsb.edu>
2024-04-04 15:22:31 -0700

    add

--- labs/welcome/code/censor.cpp
+++ labs/welcome/code/censor.cpp
@@ -5,29 +5,12 @@
 using namespace std;
 int main(int argc, char * argv[])
 {
-    if(argc >2)
-    {
-        cout<<"USAGE: censor"<<argc<<endl;
-        exit(1);
-    }
-    if(argc == 1)
+    if(argc != 2)
     {
         cout<<"USAGE: censor"<<argc<<endl;
         exit(1);
     }
     string text;
     getline(cin, text);
-    vector<string>words;
-    stringstream ss(text);
-    string word;
-    while(ss>>word){
-        words.push_back(word);
-    }
-    for(int i =0; i<words.size(); i++){
-        if((words.at(i)).size() != atoi(argv[1])){
-            cout<<words.at(i);
-        }
-    }
-    cout<<endl;
 
 }
