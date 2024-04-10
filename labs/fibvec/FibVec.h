453a7fe44541ccb402ed78a78791d76512b2b17c
Yuchao <yuchaozheng@ucsb.edu>
2024-04-10 14:03:04 -0700

    finish adding FibVec.h

--- labs/fibvec/FibVec.h
+++ labs/fibvec/FibVec.h
@@ -6,11 +6,24 @@
 
 class FibVec {
   // Member Variables
+  int *v;
+  size_t fn;
+  size_t currentsize;
 
   // Helper Functions
+  size_t fibnumber(size_t num);
+
 
 public:
   // Constructor and Destructor
+  size_t capacity()const;
+  size_t count()const;
+  void insert(int value, size_t index);
+  int lookup(size_t index)const;
+  int pop();
+  void push(int value);
+  int remove(size_t index);
+
 
   // Member Functions
 
