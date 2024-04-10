3fb8a9e8d2c178525089512842184ef3768e747c
Yuchao <yuchaozheng@ucsb.edu>
2024-04-10 14:08:29 -0700

    fibnumber

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -4,4 +4,14 @@
 #include <stdexcept>
 
 
-// FibVec Function Implementations
+// FibVec Function Implementation
+size_t FibVec::fibnumber(size_t num){
+    if(num==0){
+        return 0;
+    }
+    if(num==1 || num==2)
+    {
+        return 1;
+    }
+    return fibnumber(num-1)+fibnumber(num-2);
+}
