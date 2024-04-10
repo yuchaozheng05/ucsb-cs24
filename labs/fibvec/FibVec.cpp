c85468befca0f4b4f05511d76fb811ec8a35a295
Yuchao <yuchaozheng@ucsb.edu>
2024-04-10 16:06:39 -0700

    a

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -15,3 +15,51 @@ size_t FibVec::fibnumber(size_t num){
     }
     return fibnumber(num-1)+fibnumber(num-2);
 }
+FibVec::FibVec()
+{
+    v = new int[1];
+    fn=1;
+    currentsize=0;
+}
+FibVec::~FibVec()
+{
+    delete[]v;
+}
+size_t FibVec::capacity()const{
+    return fn;
+}
+size_t FibVec::count()const{
+    return currentsize; 
+}
+void FibVec::insert(int value, size_t index){
+    if(index>currentsize)
+    {
+        throw std::out_of_range("out of range");
+    }
+}
+int FibVec::lookup(size_t index)const{
+   if(index>currentsize)
+    {
+        throw std::out_of_range("out of range");
+    } 
+}
+int FibVec::pop(){
+    if(currentsize==0)
+    {
+        throw std::underflow_error("vector is empty");
+    }
+}
+void FibVec::push(int value){
+    insert(value, currentsize);
+}
+int FibVec::remove(size_t index){
+   if(index>currentsize)
+    {
+        throw std::out_of_range("out of range");
+    } 
+    remove(index);
+    return lookup(index); 
+}
+
+
+
