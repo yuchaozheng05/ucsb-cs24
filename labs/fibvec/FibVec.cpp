645403a48772c70a34462dc843b755ad7d13dbbd
Yuchao <yuchaozheng@ucsb.edu>
2024-04-11 13:15:53 -0700

    a

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -55,6 +55,8 @@ void FibVec::insert(int value, size_t index){
         array_[i]=array_[i-1];
     }
     array_[index]=value;
+    array_fnum = fibnumber(array_fnum);
+    array_count++;
 
 }
 int FibVec::lookup(size_t index)const{
