c07ffd7b22629177a85badd22606898b94bed76b
Yuchao <yuchaozheng@ucsb.edu>
2024-04-20 10:17:20 -0700

    yer

--- labs/tictac/Move.cpp
+++ labs/tictac/Move.cpp
@@ -3,18 +3,27 @@
 #include <cctype>
 #include <sstream>
 #include <istream>
-
 // Space for implementing Move functions.    
 
 Move::Move(const std::string &input){
     std::istringstream text(input);
-    text>>number;
-    if(number<1 || number >9)
-    {
-        throw ParseError("Wrong number");
+    unsigned walker = 0;
+    if(text>>number){
+        if (number < 1 || number > 9) {
+            throw ParseError("Number out of bound");
+        }
+    } 
+    else {
+        throw ParseError("Not an number");
     }
-    if(!isspace(input[1])){
-        throw ParseError("Wrong input");
+    walker += 1;
+    if(walker < input.size() && !isspace(input[walker])){
+        throw ParseError("Wrong input, expect white space");
+    }
+    walker += 1;
+    // consider case if multiple white spaces exist
+    if(walker < input.length() && isspace(input[walker])){
+        throw ParseError("Wrong input, multiple space");
     }
     char playerletter;
     text>>std::ws>>playerletter;
@@ -22,54 +31,63 @@ Move::Move(const std::string &input){
     player = toupper(player);
     if(player != 'X' && player != 'O')
     {
-        throw ParseError("Invalid Player");
+        throw ParseError("Invalid Player: " + player);
     }
-    if(!isspace(input[3])){
+    walker += 1;
+    if(walker < input.length() &&!isspace(input[walker])){
         throw ParseError("wrong input");
     }
+    walker += 1;
+    if (walker < input.length() && isspace(input[walker])){
+        throw ParseError("Wrong input, multiple spaces");
+    }
+    std::string rowcol;
+    text>>std::ws>>rowcol;
+    if(rowcol.size()!=2)
+    {
+        throw ParseError("wrong rowcol");
+    }
     char row_, col_;
-    text>>std::ws>>row_>>col_;
-    column = col_ -'1';
-    if(!isalpha(row_) && !isdigit(column))
+    row_ = rowcol[0];
+    col_ = rowcol[1];
+    if(!isalpha(row_)||!isdigit(col_))
     {
         throw ParseError("Invalid row or column");
     }
+    column = col_ -'1';
     row = toupper(row_) - 'A';
-    if((row !=0 && row !=1 && row!=2) || (column!=1 && column !=0 && column !=2) )
+    if((row !=0 && row !=1 && row!=2) || (column!=0 && column !=1 && column !=2) )
     {
         throw ParseError("Invalid row or colunm letter");
     }
-    if(isspace(input[5]))
+    walker += 2;
+    // mark the required field ending places
+    unsigned lastPos = walker;
+
+    // Optionally, whitespace, count if any 
+    if (walker < input.length() && isspace(input[walker]))
     {
-        throw ParseError("Invalid space");
+        walker += 1;
     }
+    //Optionally, a comment, whitespaces required
     char comment;
     text>>std::ws;
     if(text>>comment) 
     {
-        if(comment != '#'){
-            throw ParseError("Invalid comment");
-        }
         if(comment == '#')
         {
-            if(!isspace(input[6]))
+            if(lastPos == walker)
             {
+                // no preceeding whitespace
                 throw ParseError("Invalid comment space");
             }
-            if(isspace(input[7]))
-            {
-                throw ParseError("extra space on comment");
-            }
         }
-    }
-    if(input.size()>6)
-    {
-        if(!isspace(input[6]))
-        {
-            throw ParseError("extra space");
+        else{
+            throw ParseError("Wrong comment");
         }
     }
 }
+
 std::string Move::to_string()const{
     std::ostringstream text;
     text<<number<<" "<<player<<" "<<char(row+'A')<<column+1;
