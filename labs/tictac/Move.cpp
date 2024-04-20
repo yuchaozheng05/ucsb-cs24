525af0e757591c148704c820377751f0d11d2bf0
Yuchao <yuchaozheng@ucsb.edu>
2024-04-19 20:13:55 -0700

    aaa

--- labs/tictac/Move.cpp
+++ labs/tictac/Move.cpp
@@ -42,21 +42,17 @@ Move::Move(const std::string &input){
         throw ParseError("Invalid row or colunm letter");
     }
     char nextchar;
-    if(text>>nextchar){
+    if(text>>std::ws>>nextchar){
         if(nextchar!='#')
         {
             throw ParseError("Invalid comment");
         }
         if(nextchar=='#')
         {
-            text>>std::ws>>nextchar;
             if(!isspace(input[6])){
                 throw ParseError("Invalid comment space");
             }
         }
-    }
-    if(!isspace(input[8])){
-        throw ParseError("Invalid space");
     }
 }
 std::string Move::to_string()const{
