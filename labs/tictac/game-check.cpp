863fca829958fe6316414b72093941df47a6a9df
Yuchao <yuchaozheng@ucsb.edu>
2024-04-21 09:30:42 -0700

    f

--- labs/tictac/game-check.cpp
+++ labs/tictac/game-check.cpp
@@ -4,22 +4,38 @@
 #include "Move.h"
 #include <iostream>
 
-int main() {
-    Board board;
-    std::string line;
+int main(int argc, char** argv) {
+  bool verbose = false;
+
+  if(argc == 2 && std::string(argv[1]) == "-v") {
+    verbose = true;
+  }
+
+  if(verbose) {
+    std::cout << "> ";
+  }
+  Board board;
+  std::string line;
     while (std::getline(std::cin,line))
     {
         try{
             Move move(line);
             board.makeTurn(move);
+            return 0;
         }
         catch(const ParseError& e)
         {
+            if(verbose) {
+      std::cout << "Parse error: " << e.what() << '\n';
+    }
             std::cout<<"Parse error.\n";
             return 1;    
         }
         catch(const InvalidMove& e)
         {
+            if(verbose) {
+      std::cout << "InvalidMove: " << e.what() << '\n';
+    }
             std::cout<<"Invalid move.\n";
             return 2;
         }
