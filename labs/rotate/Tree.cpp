29e259c82ac3a420719f01762e112ec803bb1929
Yuchao <yuchaozheng@ucsb.edu>
2024-04-26 22:00:10 -0700

    a

--- labs/rotate/Tree.cpp
+++ labs/rotate/Tree.cpp
@@ -146,11 +146,11 @@ Node* Tree::reblanced(Node* node)
     {
         return node;
     }
-    if(balance > 1 && std::abs(inbalanced(node->left))>0)
+    if(balance > 1 && inbalanced(node->left)>0)
     {
         return rotateRight(node);
     }
-    if (balance < -1 && std::abs(inbalanced(node->right)) > 0) 
+    if (balance < -1 && inbalanced(node->right) < 0) 
     {
         return rotateLeft(node);
     }
