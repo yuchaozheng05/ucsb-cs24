1e616a3eccc1ebf3d9f8ef86a4683fcc16b3ce66
Yuchao <yuchaozheng@ucsb.edu>
2024-05-18 13:51:20 -0700

    a

--- labs/counter/Index.cpp
+++ labs/counter/Index.cpp
@@ -1,5 +1,3 @@
 #include "Index.h"
-#include <iostream>
-#include <string>
-#include "List.h"
+
 // Index Member Functions
--- labs/counter/Index.h
+++ labs/counter/Index.h
@@ -1,6 +1,6 @@
 #ifndef INDEX_H
 #define INDEX_H
-#include <vector>
+
 // TODO..
 
 #endif
