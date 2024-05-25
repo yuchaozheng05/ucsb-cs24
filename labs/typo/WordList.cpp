c0de85476246afd12e03ade86ac2ce610c601e9a
Yuchao <yuchaozheng@ucsb.edu>
2024-05-25 16:59:06 -0700

    a

--- labs/typo/WordList.cpp
+++ labs/typo/WordList.cpp
@@ -2,7 +2,23 @@
 
 WordList::WordList(std::istream& stream)
 {
-    return;
+    std::string word;
+    while(stream >> word)
+    {
+        bool is_lower = true;
+        for(char char_:word)
+        {
+            if(!islower(char_))
+            {
+                is_lower = false;
+                break;
+            }
+        }
+        if(is_lower)
+        {
+            mWords.push_back(word);
+        }
+    }
 }
 
 Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const
