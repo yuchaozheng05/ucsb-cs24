1532f64186714e390d4c5a4c3adca0d90f671221
Yuchao <yuchaozheng@ucsb.edu>
2024-05-26 18:21:42 -0700

    a

--- labs/typo/WordList.cpp
+++ labs/typo/WordList.cpp
@@ -50,13 +50,9 @@ Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float 
         float score_ = (score/points.size());
         if(score_>=cutoff)
         {
-            if(heap.count()<=maxcount)
+            if(heap.count()<maxcount)
             {
                 heap.push(word,score_);
-            }
-            else if(score_>heap.top().score)
-            {
-                heap.pushpop(word,score_);
             }
         }
     }
