272098ef0d2282aae51542feb916792193981a26
Yuchao <yuchaozheng@ucsb.edu>
2024-06-06 16:31:54 -0700

    a

--- labs/voxhop/Point.h
+++ labs/voxhop/Point.h
@@ -26,7 +26,17 @@ inline bool operator<(const Point& lhs, const Point& rhs) {
     if (lhs.y != rhs.y) return lhs.y < rhs.y;
     return lhs.x < rhs.x;
 }
-
+namespace std {
+    template<> struct hash<Point> {
+        size_t operator()(const Point& point) const {
+            size_t h1 = std::hash<int>{}(point.x);
+            size_t h2 = std::hash<int>{}(point.y);
+            size_t h3 = std::hash<int>{}(point.z);
+            // Combine hashes
+            return h1 ^ (h2 << 1) ^ (h3 << 2); // Shift and XOR for simple combination
+        }
+    };
+}
 std::istream& operator >> (std::istream& stream, Point& point);
 std::ostream& operator << (std::ostream& stream, const Point& point);
 
